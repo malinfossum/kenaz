ff7c259af4463fa752109f4f8a5648654bbf2006
Malin Fossum <malinfossum.dev@proton.me>
2026-08-13 19:36:12 +0200

    build(brand): upgrade design-system 1.0.0 to 2.0.2 and bump the SW cache

--- Kenaz.Web/public/sw.js
+++ Kenaz.Web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kenaz-v1"
+const CACHE = "kenaz-v2"
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"]
 
 self.addEventListener("install", (event) => {
